c9053769fd51b8f6d8af5a7e76c9b2640df80dd4
Gustavo Quiciri Goncalves <1056097@senacminas.edu.br>
2025-07-03 16:51:40 -0300

    Primeira versão do jogo

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+def jogar():
+    numero = random.randint(1, 20)
+    tentativas = 0
+    print("=== Jogo de Adivinhação ===")
+    print("Adivinhe um número entre 1 e 20")
+
+    while True:
+        palpite = int(input("Seu palpite: "))
+        tentativas += 1
+
+        if palpite < numero:
+            print("O número é MAIOR")
+        elif palpite > numero:
+            print("O número é MENOR")
+        else:
+            print(f"🎉 Acertou em {tentativas} tentativa(s)!")
+            break
+
+jogar()
+
